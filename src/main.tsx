9b8be59a3fa47a23c2f7c0fc998d6bf1d8ed4007
Jordan Skomal <jskomal@gmail.com>
2022-09-26 17:21:06 -0600

    Add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+const router = createBrowserRouter([{ path: '/', element: <App /> }])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <RouterProvider router={router} />
+)
