38356891ff0a86da55f1e4a7a25055a90c8db40f
Jordan Skomal <jskomal@gmail.com>
2022-09-26 17:20:54 -0600

    Add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from './components/Header'
+import { user, checkingAccount, savingsAccount } from './assets/data'
 function App() {
-  return <div className='app'></div>
+  const [userInfo, setUserInfo] = useState(user)
+  const [checking, setChecking] = useState(checkingAccount)
+  const [savings, setSavings] = useState(savingsAccount)
+
+  const handleNotifications = () => {}
+
+  return (
+    <div className='app'>
+      <Header userInfo={userInfo} handleNotifications={handleNotifications} />
+    </div>
+  )
 }
 
 export default App
