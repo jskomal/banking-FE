778ceb91b8be5d6f2b0a3dcdd2269f9d28a88256
Jordan Skomal <jskomal@gmail.com>
2022-10-01 05:01:10 -0600

    Add Routing for Children

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import Transfer from './components/Transfer'
+import Home from './components/Home'
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [{ path: '/', element: <Home /> }]
+  },
+  {
+    path: 'transfer',
+    element: <App />,
+    children: [{ path: 'transfer', element: <Transfer /> }]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RouterProvider router={router} />
