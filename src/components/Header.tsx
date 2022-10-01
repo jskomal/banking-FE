75500210abf177de8a9081378d3f99b571654a8d
Jordan Skomal <jskomal@gmail.com>
2022-10-01 02:06:01 -0600

    Add active class to the current page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import React from 'react'
+import dayjs from 'dayjs'
 import reactLogo from '../assets/react.svg'
 import bell from '../assets/bell.svg'
-import { Outlet, Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './Header.css'
 
 import { TUser } from '../assets/data'
@@ -14,6 +13,8 @@ type HeaderProps = {
 
 function Header({ userInfo, handleNotifications }: HeaderProps) {
   const { firstName, lastName, notificationCount, memberSince } = userInfo
+
+  const { pathname } = useLocation()
 
   const initals = firstName[0].toUpperCase() + lastName[0].toUpperCase()
 
@@ -27,9 +28,15 @@ function Header({ userInfo, handleNotifications }: HeaderProps) {
             <p>Credit Union</p>
           </div>
         </div>
-        <Link to={'/'}>My Accounts {'\u2304'}</Link>
-        <Link to={'transfer'}>Move Money {'\u2304'}</Link>
-        <Link to={'analytics'}>Analytics {'\u2304'}</Link>
+        <Link to={'/'} className={pathname === '/' ? 'active' : ''}>
+          My Accounts {'\u2304'}
+        </Link>
+        <Link to={'transfer'} className={pathname === '/transfer' ? 'active' : ''}>
+          Move Money {'\u2304'}
+        </Link>
+        <Link to={'analytics'} className={pathname === '/analytics' ? 'active' : ''}>
+          Analytics {'\u2304'}
+        </Link>
         <div className='notification'>
           <img src={bell} alt='notification' />
           <p>{notificationCount}</p>
@@ -40,7 +47,7 @@ function Header({ userInfo, handleNotifications }: HeaderProps) {
             <h3>
               {firstName.toUpperCase()} {lastName.toUpperCase()}
             </h3>
-            <p>Member Since {memberSince}</p>
+            <p>Member Since {dayjs(memberSince).format('MMM D, YYYY')}</p>
           </div>
         </div>
       </div>
