ed5448675b2aa8ed45e2b03651d5496e6149a43f
Jordan Skomal <jskomal@gmail.com>
2022-10-04 19:30:00 -0600

    Create Manage Component

--- src/components/Manage.tsx
+++ src/components/Manage.tsx
@@ -9,7 +9,45 @@ type ManageProps = {
 }
 
 function Manage({ userInfo, setUserInfo }: ManageProps) {
-  return <div>Manage</div>
+  const handleCheckingVisibility = () => {
+    setUserInfo((prev) => {
+      prev.settings.isCheckingShown = !prev.settings.isCheckingShown
+      return { ...prev }
+    })
+  }
+
+  const handleSavingsVisibility = () => {
+    setUserInfo((prev) => {
+      prev.settings.isSavingsShown = !prev.settings.isSavingsShown
+      return { ...prev }
+    })
+  }
+
+  return (
+    <div className='manage-view'>
+      <div className='manage-container'>
+        <h2>Manage Account Visibility</h2>
+        <div className='input-pair'>
+          <span>Show Checking Account</span>
+          <input
+            className='checkbox'
+            type='checkbox'
+            checked={userInfo.settings.isCheckingShown}
+            onChange={handleCheckingVisibility}
+          />
+        </div>
+        <div className='input-pair'>
+          <span>Show Savings Account</span>
+          <input
+            className='checkbox'
+            type='checkbox'
+            checked={userInfo.settings.isSavingsShown}
+            onChange={handleSavingsVisibility}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Manage
