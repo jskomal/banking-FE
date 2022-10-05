b8b638ec780513a891426efb2b1c2c3048a3c2ac
Jordan Skomal <jskomal@gmail.com>
2022-10-04 19:29:43 -0600

    Add a message if all accounts are hidden

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import AccountPreview from './AccountPreview'
 import QuickActions from './QuickActions'
 
 import type { TAccount, TUser } from '../assets/data'
+import { Link } from 'react-router-dom'
 
 type HomeProps = {
   checking: TAccount
@@ -16,6 +17,15 @@ function Home({ checking, savings, userInfo }: HomeProps) {
         <h2 className='title'>My Accounts</h2>
         {userInfo.settings.isCheckingShown && <AccountPreview account={checking} />}
         {userInfo.settings.isSavingsShown && <AccountPreview account={savings} />}
+        {!userInfo.settings.isCheckingShown && !userInfo.settings.isSavingsShown && (
+          <p>
+            No Accounts Shown, check the{' '}
+            <Link className='link' to='/manage'>
+              Manage Accounts page
+            </Link>{' '}
+            to display an account
+          </p>
+        )}
       </div>
       <div>
         <QuickActions />
