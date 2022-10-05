9c6638c10470d1a5fa0ffbedf1487e411fe42130
Jordan Skomal <jskomal@gmail.com>
2022-10-04 19:00:01 -0600

    Add items to Quick Actions

--- src/components/QuickActions.tsx
+++ src/components/QuickActions.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import './QuickActions.css'
 
-type Props = {}
-
-function QuickActions({}: Props) {
-  return <div>QuickActions</div>
+function QuickActions() {
+  return (
+    <div className='quick-container'>
+      <h2 className='title'>Quick Actions</h2>
+      <section className='quick-actions-container'>
+        <div className='list'>
+          <Link to='/manage' className='link'>
+            Manage Account
+          </Link>
+          <Link to='/findus' className='link'>
+            Find Us
+          </Link>
+          <Link to='/contact' className='link'>
+            Contact us
+          </Link>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default QuickActions
